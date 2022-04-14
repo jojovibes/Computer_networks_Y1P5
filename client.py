c801b4252f7742e5f8835c81e45480d23ea0a91e
[didepoyraz] <[didepyz@gmail.com]>
2022-04-14 09:49:04 +0200

    e

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host_port = ("143.47.184.219")
+host_port = ("143.47.184.219", 5378)
 sock.connect(host_port)
 
 sock.sendall(b"Hello World")
