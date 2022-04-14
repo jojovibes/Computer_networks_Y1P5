c1b7537e3b868e738fd36fac12f266dac84d229c
[didepoyraz] <[didepyz@gmail.com]>
2022-04-14 12:22:56 +0200

    w

--- client.py
+++ client.py
@@ -15,10 +15,20 @@ newline = " \n".encode("utf-8")
 
 test_hello = handshake + username + newline
 user_socket.sendall(test_hello)
-print ("server:", user_socket.recv(4096))
+
+
+data = user_socket.recv(4096).decode("utf-8")
+print("server" + data)
+
+if data == "IN-USE\n":
+    new_name = input("new username: ")
+    second_username = new_name.encode("utf-8")
+    print((handshake + second_username + newline))
+    user_socket.sendall(handshake + second_username + newline)
+    print(user_socket.recv(4096))
+    data = user_socket.recv(4096) 
 
 who = "WHO\n".encode("utf-8")
-send = 
 
 while True:
         my_message = input("send: ")
@@ -29,9 +39,7 @@ while True:
 
         if(my_message == "!who"):
             user_socket.sendall(who)
-        
-        if(my_message[0] == "@"):
-            
+
 
         print ("server:", user_socket.recv(4096))
 user_socket.close()
